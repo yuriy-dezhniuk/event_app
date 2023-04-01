6a574a52df9b04ce928ff736ab64284d8fbdfa93
Yurii Dezhniuk <yuriidezhniuk@Yuriis-Mac-mini.local>
2023-04-01 21:50:26 +0200

    Create main empty pages with titles

--- event_app_container/events_app/views.py
+++ event_app_container/events_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, this is the page with full list of events")
+
+
+def event(request):
+    return HttpResponse("Hello, this is the page with separate event")
+
+def create_event(request):
+    return HttpResponse("Hello, this is the page for creating new events")
