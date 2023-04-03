9eada4627841df9c5e748bd37d58983b1d90c699
Yurii Dezhniuk <yuriidezhniuk@Yuriis-Mac-mini.local>
2023-04-03 21:15:23 +0200

    CSS and HTML base files already created

--- event_app_container/events_app/views.py
+++ event_app_container/events_app/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, this is the page with full list of events")
-
+    return render(request, 'events_app/list_of_events.html')
 
 def event(request):
     return HttpResponse("Hello, this is the page with separate event")
 
 def create_event(request):
     return HttpResponse("Hello, this is the page for creating new events")
+
+
